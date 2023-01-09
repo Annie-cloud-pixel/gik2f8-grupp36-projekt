f775684579245b67526f5fbf92581f2905f20f99
Jessielundin <96108658+Jessielundin@users.noreply.github.com>
2023-01-09 16:01:47 +0100

    Få sidan att fungera
    Funktioner för det mesta. Kunna ta bort, lägga till. Ändra utseende på sidan etcetera.

--- server/app.js
+++ server/app.js
@@ -1,20 +1,35 @@
+/* Importrerar nodemodulen express (installerad med npm), som är ett utbrett verktyg för att skapa och arbeta med webbservrar och hantera HTTP-förfrågningar i ett nodejs-backend. */
 const express = require('express');
-/* Skapar upp ett express-objekt, som representerar en webbserver */
+/* Skapar upp ett express-objekt, som i stort representerar en webbserver */
 const app = express();
+
+/* Importerar den inbyggda modulen fs */
 const fs = require('fs/promises');
 
-const PORT = 5000; 
-
-app.use(express.json()).use(express.urlencoded({extended: false }))
-.use((req, res) => {
-    res.header('Access-Cobtrol-Allow-Origin', '*');
+const PORT = 5000;
+/* Expressobjektet, kallat app, har metoden "use" som används för att sätta inställningar hos vår server */
+app
+  /* Man kan ange format etc. på de data som servern ska kunna ta emot och skicka. Metoderna json och urlencoded är inbyggda hos express */
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  /* Man kan också ange vad som ska hända övergripande med samtliga förfrågningar. Alla förfrågningar kommer att gå genom nedanstående kod först, innan den behandlas vidare. */
+  .use((req, res, next) => {
+    /* Det vill säga, alla response-objekt kommer att få nedanstående headers. */
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Methods', '*');
     /* För att göra så att servern ska kunna behandla förfrågan vidare, använder man funktionen next() som kommer som tredje parameter till denna callbackfunktion.  */
     next();
   });
 
-/* GET method route */
+/* Express (som finns i variabeln app) har metoder för att specificera vad som ska hända vid olika HTTP-metoder. Man anropar metoden get() hos expressobjektet för att fånga upp förfrågningar med metoden GET - alltså en GET-förfrågan -  från en klient.  */
+
+/* .get tar emot
+  1. En route, som utgör en del av adressen/URL:en dit man kan skicka förfrågan. Man anger det som ska stå efter domän och port (vår server är konfigurerar som default att köra på localhost:5000), så här metod lyssnar man alltså efter GET-anrop till url:en localhost:5000/task
+  
+  Notera att route-namnen döps om i lektion 6. De ska heta tasks, inte task, men felet är enligt videorna inte tillrättat i detta skede, så jag lämnar kvar det. 
+
+  2. En callbackfunktion som kommer att köras när en sådan förfrågan görs. Callbackfunktionen tar (minst) två parametrar - ett requestobjekt och ett responseobjekt, som här kallas req och res. Callbackfunktionen är asynkron för att vi använder await inuti. */
 app.get('/tasks', async (req, res) => {
   /* För enkel felhantering används try/catch */
   try {
@@ -27,7 +42,7 @@ app.get('/tasks', async (req, res) => {
     res.status(500).send({ error });
   }
 });
-/* POST method route */
+/* Express metod för att lyssna efter POST-anrop heter naturligt post(). I övrigt fungerar den likadant som  get */
 app.post('/tasks', async (req, res) => {
   try {
     /* Alla data från klienten finns i req-objektet. I req.body finns alla data, alltså själva innehållet i förfrågan. I detta fall den uppgift som ska sparas ned. */
@@ -66,7 +81,7 @@ app.post('/tasks', async (req, res) => {
 });
 /* Express metod för att lyssna efter DELETE-anrop heter naturligt delete(). I övrigt fungerar den likadant som get och post */
 
-/* DELETE method route */
+/* Route-adressen som specificeras i delete har /:id i tillägg till adressen. Det betyder att man i adressen kan skriva task följt av ett / och sedan något som kommer att sparas i en egenskap vid namn id. :id betyder att det som står efter / kommer att heta id i requestobjektet. Hade kunnat vara vad som helst. Så här möjliggörs att lyssna efter DELETE-anrop på exempelvis url:en localhost:5000/task/1 där 1 då skulle motsvara ett id på den uppgift man vill ta bort */
 app.delete('/tasks/:id', async (req, res) => {
   console.log(req);
   try {
@@ -98,29 +113,11 @@ app.delete('/tasks/:id', async (req, res) => {
   }
 });
 
-/* PATCH för att kunna uppdatera innehåll */
-app.patch("/tasks/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const updatedTask = req.body;
+/***********************Labb 2 ***********************/
+/* Här skulle det vara lämpligt att skriva en funktion som likt post eller delete tar kan hantera PUT- eller PATCH-anrop (du får välja vilket, läs på om vad som verkar mest vettigt för det du ska göra) för att kunna markera uppgifter som färdiga. Den nya statusen - completed true eller falase - kan skickas i förfrågans body (req.body) tillsammans med exempelvis id så att man kan söka fram en given uppgift ur listan, uppdatera uppgiftens status och till sist spara ner listan med den uppdaterade uppgiften */
 
-    const listBuffer = await fs.readFile("./tasks.json");
-    const currentTasks = JSON.parse(listBuffer);
-    if (currentTasks.length > 0) {
-      const updatedList = currentTasks.map((task) => {
-        if (task.id == id) {
-          return { ...task, ...updatedTask };
-        } else {
-          return task;
-        }
-      });
+/* Observera att all kod rörande backend för labb 2 ska skrivas i denna fil och inte i app.node.js. App.node.js är bara till för exempel från lektion 5 och innehåller inte någon kod som används vidare under lektionerna. */
+/***********************Labb 2 ***********************/
 
-      await fs.writeFile("./tasks.json", JSON.stringify(updatedList));
-      res.send({ message: `Uppgift med id ${id} uppdaterad` });
-    } else {
-      res.status(404).send({ error: "Ingen uppgift att uppdatera" });
-    }
-  } catch (error) {
-    res.status(500).send({ error: error.stack });
-  }
-});
+/* Med app.listen säger man åte servern att starta. Första argumentet är port - dvs. det portnummer man vill att servern ska köra på. Det sattes till 5000 på rad 9. Det andra argumentet är en anonym arrow-funktion som körs när servern har lyckats starta. Här skrivs bara ett meddelande ut som berättar att servern kör, så att man får feedback på att allt körts igång som det skulle. */
+app.listen(PORT, () => console.log('Server running on http://localhost:5000'));
